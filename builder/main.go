541e9c4726921cd4e3c6cb6ab0ddbebbe374e3d1
Linus Unnebäck <linus@folkdatorn.se>
2017-04-21 23:55:08 +0200

    🚧 Work in progress

--- builder/main.go
+++ builder/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+
 	"github.com/gopherjs/gopherjs/js"
 	"github.com/hashicorp/hcl"
+	"github.com/hashicorp/hcl/hcl/printer"
+	jsonParser "github.com/hashicorp/hcl/json/parser"
 )
 
 func parse(src []byte) (interface{}, error) {
@@ -16,6 +22,30 @@ func parse(src []byte) (interface{}, error) {
 	return result, nil
 }
 
+func stringify(src interface{}) ([]byte, error) {
+	var err error
+
+	json, err := json.Marshal(src)
+	if err != nil {
+		return nil, err
+	}
+
+	ast, err := jsonParser.Parse(json)
+	if err != nil {
+		return nil, err
+	}
+
+	var result bytes.Buffer
+
+	err = printer.Fprint(bufio.NewWriter(&result), ast)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.Bytes(), nil
+}
+
 func main() {
 	js.Module.Get("exports").Set("parse", parse)
+	js.Module.Get("exports").Set("stringify", stringify)
 }
